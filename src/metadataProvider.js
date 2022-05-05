41bdd8049d7f87b47b6f43f0b126eb604b776599
CarlosRoa <carlosroa@gmail.com>
2022-05-05 16:47:19 -0400

    Back to vers

--- src/metadataProvider.js
+++ src/metadataProvider.js
@@ -5,94 +5,75 @@ import dicomParser from 'dicom-parser';
 const { getNumberValue, getValue } = cornerstoneWADOImageLoader.wadors.metaData;
 
 function wadoRsMetaDataProvider(type, imageId) {
-  
-  metaURL = imageId.split('wadors:')[1]
-  
-  let xhr = new XMLHttpRequest();
-   xhr.onreadystatechange = function() {
-     if (this.readyState == 4 && this.status == 200) {
-            // Make sure it's a JSON document
-            let instancesData = JSON.parse(this.responseText);
-       
-             cornerstoneWADOImageLoader.wadors.metaDataManager.add(imageId, instancesData[0])
-        }
+  const metaData = cornerstoneWADOImageLoader.wadors.metaDataManager.get(
+    imageId
+  );
+
+  if (!metaData) {
+    return;
+  }
+
+  if (
+    metaData[type] !== undefined &&
+    metaData[type].Value !== undefined &&
+    metaData[type].Value.length
+  ) {
+    return metaData[type].Value[0];
+  }
+
+  const typeCleaned = type.replace('x', '');
+  if (
+    metaData[typeCleaned] !== undefined &&
+    metaData[typeCleaned].Value !== undefined &&
+    metaData[typeCleaned].Value.length
+  ) {
+    return metaData[typeCleaned].Value[0];
+  }
+
+  if (type === 'generalImageModule') {
+    return {
+      sopInstanceUid: getValue(metaData['00080018']),
+      instanceNumber: getNumberValue(metaData['00200013']),
+      lossyImageCompression: getValue(metaData['00282110']),
+      lossyImageCompressionRatio: getValue(metaData['00282112']),
+      lossyImageCompressionMethod: getValue(metaData['00282114']),
     };
+  }
 
-    xhr.open("GET", metaURL, true);
-    xhr.setRequestHeader('Accept', 'application/json');
-    return xhr.onloadend = function() {
-      
-      const metaData = cornerstoneWADOImageLoader.wadors.metaDataManager.get(
-       imageId
-      );
-
-      if (!metaData) {
-        return;
-      }
-
-      if (
-        metaData[type] !== undefined &&
-        metaData[type].Value !== undefined &&
-        metaData[type].Value.length
-      ) {
-        return metaData[type].Value[0];
-      }
-
-      const typeCleaned = type.replace('x', '');
-      if (
-        metaData[typeCleaned] !== undefined &&
-        metaData[typeCleaned].Value !== undefined &&
-        metaData[typeCleaned].Value.length
-      ) {
-        return metaData[typeCleaned].Value[0];
-      }
-
-      if (type === 'generalImageModule') {
-        return {
-          sopInstanceUid: getValue(metaData['00080018']),
-          instanceNumber: getNumberValue(metaData['00200013']),
-          lossyImageCompression: getValue(metaData['00282110']),
-          lossyImageCompressionRatio: getValue(metaData['00282112']),
-          lossyImageCompressionMethod: getValue(metaData['00282114']),
-        };
-      }
-
-    if (type === 'patientModule') {
-        const patientValue = getValue(metaData['00100010']);
-        let patientName;
-        if (typeof patientValue === 'string') {
-          patientName = patientValue;
-        } else if (typeof patientValue === 'object') {
-          patientName = patientValue.Alphabetic;
-        }
-        return {
-          patientName,
-          patientId: getValue(metaData['00100020']),
-          patientSex: getValue(metaData['00100040']),
-          patientBirthDate: getValue(metaData['00100030']),
-        };
-      }
-
-      if (type === 'spacingBetweenSlices') {
-        return getValue(metaData['00180088']);
-      }
-
-      if (type === 'generalStudyModule') {
-        return {
-          studyDescription: getValue(metaData['00081030']),
-          studyDate: getValue(metaData['00080020']),
-          studyTime: getValue(metaData['00080030']),
-          accessionNumber: getValue(metaData['00080050']),
-        };
-      }
-
-      if (type === 'cineModule') {
-        return {
-          frameTime: getNumberValue(metaData['00181063']),
-        };
-      }
+if (type === 'patientModule') {
+    const patientValue = getValue(metaData['00100010']);
+    let patientName;
+    if (typeof patientValue === 'string') {
+      patientName = patientValue;
+    } else if (typeof patientValue === 'object') {
+      patientName = patientValue.Alphabetic;
     }
-    xhr.send();
+    return {
+      patientName,
+      patientId: getValue(metaData['00100020']),
+      patientSex: getValue(metaData['00100040']),
+      patientBirthDate: getValue(metaData['00100030']),
+    };
+  }
+
+  if (type === 'spacingBetweenSlices') {
+    return getValue(metaData['00180088']);
+  }
+
+  if (type === 'generalStudyModule') {
+    return {
+      studyDescription: getValue(metaData['00081030']),
+      studyDate: getValue(metaData['00080020']),
+      studyTime: getValue(metaData['00080030']),
+      accessionNumber: getValue(metaData['00080050']),
+    };
+  }
+
+  if (type === 'cineModule') {
+    return {
+      frameTime: getNumberValue(metaData['00181063']),
+    };
+  }
 }
 
 cornerstone.metaData.addProvider(wadoRsMetaDataProvider);
